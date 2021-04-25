d476c0c7cd359c741603ce607b30bb4f9d893954
Juan <juan.gallegomesa@gmail.com>
2021-04-25 07:50:41 -0500

    se crea app multiidioma

--- app/src/main/java/com/example/loginsabado/Home.java
+++ app/src/main/java/com/example/loginsabado/Home.java
@@ -5,16 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class Home extends AppCompatActivity {
 
+    //ATRIBUTOS
+
     ArrayList<Trabajador> listaDeDatos=new ArrayList<>();
     RecyclerView listado;
 
+    //METODOS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,6 @@ public class Home extends AppCompatActivity {
         listado.setAdapter(adaptador);
 
     }
-
 
 
     private void crearListado() {
@@ -72,6 +78,60 @@ public class Home extends AppCompatActivity {
 
 
     }
+
+
+    //Metodo para configurar el idioma de mi telefono
+    public void cambiarIdioma(String lenguaje){
+
+        Locale idioma=new Locale(lenguaje); //tipo de dato local que recibe el idioma deseado
+        Locale.setDefault(idioma); //se establece el idioma deseado
+
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=idioma;
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+    }
+
+
+    //Metodo que pinta el Menu en ESTA actividad
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //Metodo que controla la acción de cada opción del menu
+    public boolean onOptionsItemSelected(MenuItem opcion){
+        int id=opcion.getItemId();
+
+        switch(id){
+
+            case (R.id.opcion1):
+
+                Intent intent=new Intent(Home.this,Perfil.class);
+                startActivity(intent);
+
+                break;
+
+            case (R.id.opcion2):
+                this.cambiarIdioma("en");
+                Intent intent2=new Intent(Home.this,Home.class);
+                startActivity(intent2);
+
+
+                break;
+
+            case (R.id.opcion3):
+                this.cambiarIdioma("es");
+                Intent intent3=new Intent(Home.this,Home.class);
+                startActivity(intent3);
+
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(opcion);
+    }
+
+
 
 
 }
